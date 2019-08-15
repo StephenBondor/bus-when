19e026d51b86a339718b735cb668b8950e7c4abf
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-08-14 22:41:17 -0700

    Refactor seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -8,9 +8,10 @@ const ROUTE_DEPARTURE_INTERVAL = 2;
 const ROUTE_RESTART_INTERVAL = 15;
 const TIME_BETWEEN_STOPS = 2;
 const BUS_BUFFER_WINDOW = 60;
-
 const DB_DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS';
 const DB_DATE_FORMAT_ROUNDED = `${DB_DATE_FORMAT.slice(0, 15)}:00.000Z`;
+const STARTING_BUS_AMOUNT =
+	(BUS_BUFFER_WINDOW / ROUTE_RESTART_INTERVAL) * NUMBER_OF_ROUTES;
 
 async function main() {
 	console.log('usage: ');
@@ -21,8 +22,9 @@ async function main() {
 		"'node seeder d f ' - will delete all data in the database and NOT repopulate"
 	);
 
+	// Handle deletions
 	if (process.argv[2] === 'd') {
-		console.log('deleting everything...');
+		console.log('\nDeleting everything...');
 		try {
 			await prisma.deleteManyEvents({id_not: '1'});
 			await prisma.deleteManyBuses({id_not: '1'});
@@ -34,6 +36,8 @@ async function main() {
 		} catch {
 			console.log('Was not able to delete everything in the DB');
 		}
+	} else {
+		console.log('\nPopulating DB...');
 	}
 
 	// Create all stops
@@ -43,27 +47,24 @@ async function main() {
 				name: (stop + 1).toString()
 			});
 		}
-		// console.log('stops: ', await prisma.stops());
 		console.log('\n - stops seeded successfully');
 	} catch {
 		console.log(' - stops already seeded successfully');
 	}
 
 	// Create all routes, each with a full stop list
+	let stopList = await prisma.stops();
 	try {
 		for (let route = 0; route < NUMBER_OF_ROUTES; route++) {
 			await prisma.createRoute({
 				name: (route + 1).toString(),
 				stopList: {
-					connect: [
-						...(await prisma.stops()).map(stop => ({
-							id: stop.id
-						}))
-					]
+					connect: stopList.map(stop => ({
+						id: stop.id
+					}))
 				}
 			});
 		}
-		// console.log('routes: ', await prisma.routes());
 		console.log(' - routes seeded successfully');
 	} catch {
 		console.log(' - routes already seeded successfully');
@@ -74,24 +75,21 @@ async function main() {
 	let now = moment().format('mm');
 	let subtract = (now % ROUTE_RESTART_INTERVAL) + ROUTE_RESTART_INTERVAL;
 	let startTime = moment().subtract(subtract, 'minutes');
-
-	if (
-		(await prisma.buses()).length <
-		(BUS_BUFFER_WINDOW / ROUTE_RESTART_INTERVAL) * NUMBER_OF_ROUTES
-	) {
+	const addBus = async (addedTime, route) =>
+		await prisma.createBus({
+			startTime: startTime
+				.clone()
+				.add(addedTime, 'minutes')
+				.format(DB_DATE_FORMAT_ROUNDED),
+			route: {connect: {name: (route + 1).toString()}}
+		});
+	if ((await prisma.buses()).length < STARTING_BUS_AMOUNT) {
 		for (let i = 0; i < BUS_BUFFER_WINDOW; i = i + ROUTE_RESTART_INTERVAL) {
 			for (let route = 0; route < NUMBER_OF_ROUTES; route++) {
 				addedTime = ROUTE_DEPARTURE_INTERVAL * route + i;
-				await prisma.createBus({
-					startTime: startTime
-						.clone()
-						.add(addedTime, 'minutes')
-						.format(DB_DATE_FORMAT_ROUNDED),
-					route: {connect: {name: (route + 1).toString()}}
-				});
+				await addBus(addedTime, route);
 			}
 		}
-		// console.log('buses: ', await prisma.buses());
 		console.log(' - buses seeded successfully');
 	} else {
 		console.log(' - buses already seeded successfully');
@@ -100,75 +98,54 @@ async function main() {
 	// Create events for all starting busses
 	let time;
 	let oldTime;
+	const addEvents = async bus => {
+		for (let stop = 0; stop < NUMBER_OF_STOPS; stop++) {
+			oldTime = moment(bus.startTime, DB_DATE_FORMAT);
+			time = oldTime.add(stop * TIME_BETWEEN_STOPS, 'minute');
+			await prisma.createEvent({
+				stop: {connect: {name: (stop + 1).toString()}},
+				bus: {connect: {id: bus.id}},
+				time: `${time.format(DB_DATE_FORMAT)}Z`
+			});
+		}
+	};
 	if (
 		(await prisma.events()).length <
-		(BUS_BUFFER_WINDOW / ROUTE_RESTART_INTERVAL) *
-			NUMBER_OF_ROUTES *
-			NUMBER_OF_STOPS
+		STARTING_BUS_AMOUNT * NUMBER_OF_STOPS
 	) {
 		const buses = await prisma.buses();
-		buses.map(async bus => {
-			for (let stop = 0; stop < NUMBER_OF_STOPS; stop++) {
-				oldTime = moment(bus.startTime, DB_DATE_FORMAT);
-				time = oldTime.add(stop * TIME_BETWEEN_STOPS, 'minute');
-				await prisma.createEvent({
-					stop: {connect: {name: (stop + 1).toString()}},
-					bus: {connect: {id: bus.id}},
-					time: `${time.format(DB_DATE_FORMAT)}Z`
-				});
-			}
-		});
-		// console.log('events: ', await prisma.events());
+		buses.map(bus => addEvents(bus));
 		console.log(' - events seeded successfully');
 	} else {
 		console.log(' - events already seeded successfully');
 	}
 
+	// Initial Seeding finished
 	console.log(
-		'\nSeeding successful!\n\n',
-		'Now entering continuous deployment every',
+		'\nSeeding successful!\n\nNow entering continuous deployment every',
 		ROUTE_RESTART_INTERVAL,
-		'minutes... ( control + c to Quit )'
+		'minutes... ( control + c to Quit )\n'
 	);
 
 	// Ongoing seeding
 	let additionalTime = BUS_BUFFER_WINDOW;
 	let newBusList = [];
-
 	let id = setInterval(async () => {
 		// Add new buses
 		for (let route = 0; route < NUMBER_OF_ROUTES; route++) {
 			addedTime = ROUTE_DEPARTURE_INTERVAL * route + additionalTime;
-			newBusList.push(
-				await prisma.createBus({
-					startTime: startTime
-						.clone()
-						.add(addedTime, 'minutes')
-						.format(DB_DATE_FORMAT_ROUNDED),
-					route: {connect: {name: (route + 1).toString()}}
-				})
-			);
+			newBusList.push(await addBus(addedTime, route));
 		}
 
 		// Add new events for those buses
-		newBusList.map(async bus => {
-			for (let stop = 0; stop < NUMBER_OF_STOPS; stop++) {
-				oldTime = moment(bus.startTime, DB_DATE_FORMAT);
-				time = oldTime.add(stop * TIME_BETWEEN_STOPS, 'minute');
-				await prisma.createEvent({
-					stop: {connect: {name: (stop + 1).toString()}},
-					bus: {connect: {id: bus.id}},
-					time: `${time.format(DB_DATE_FORMAT)}Z`
-				});
-			}
-		});
+		newBusList.map(bus => addEvents(bus));
 
+		// Alert / Reset / increment
 		console.log(
-			'Fresh buses scheduled and queued for period ',
+			' - Fresh buses scheduled and queued for period',
 			additionalTime,
-			' min from start'
+			'min from start'
 		);
-		// Reset / increment
 		newBusList = [];
 		additionalTime = additionalTime + ROUTE_RESTART_INTERVAL;
 	}, 1000 * 60 * ROUTE_RESTART_INTERVAL);
